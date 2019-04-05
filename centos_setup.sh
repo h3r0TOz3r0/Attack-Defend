b151f0c397ccad0640aea3ff99701816fb890314
z3r0 <anna.devries4@outlook.com>
2019-04-05 16:03:38 -0400

    udpate

--- centos_setup.sh
+++ centos_setup.sh
@@ -6,5 +6,18 @@ yum upgrade
 yum clean all
 
 ## Download required tools
-yum install nano net-tools lsof bash-completion
+yum install nano net-tools lsof bash-completion wireshark-qt
+
+## Update Kernel
+echo "Indictate if you'd like to upgrade the kernel with a y/n. Recommend upgrade if version <4.1"
+uname -r
+read -n 1 response 
+if [ response == "y" ]
+then
+	rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+	rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
+ 	yum --disablerepo="*" --enablerepo="elrepo-kernel" list available
+ 	yum --enablerepo=elrepo-kernel install kernel-ml
+fi
+
 
