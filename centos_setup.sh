603562da3c7827e9c904ebde04aee2e7c4ce948f
z3r0 <anna.devries4@outlook.com>
2019-04-05 20:40:06 -0400

    update

--- centos_setup.sh
+++ centos_setup.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/bash
 
+############################################################################
+## Instructions for Moving Script from OPS to Remote                      ##
+## scp <src> <dst>                                                        ##
+## scp centos-setup.sh bytesforeveryone@10.15.0.56:/home/bytesforeveryone ##
+############################################################################
+
 ## Update Box
 yum check-update
 yum upgrade
 yum clean all
 
 ## Download required tools
-yum install nano net-tools lsof bash-completion wireshark-qt
+yum install nano lsof tcpdump iptables 
 
 ## Update Kernel
-echo "Indictate if you'd like to upgrade the kernel with a y/n. Recommend upgrade if version <4.1"
 uname -r
-read -n 1 response 
-if [ response == "y" ]
-then
-	rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
-	rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
- 	yum --disablerepo="*" --enablerepo="elrepo-kernel" list available
- 	yum --enablerepo=elrepo-kernel install kernel-ml
-fi
 
+rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
+yum --disablerepo="*" --enablerepo="elrepo-kernel" list available
+yum --enablerepo=elrepo-kernel install kernel-ml
 
+reboot
