bbfc0733dde168d94660c3cc58aa74c577e9ae19
Anna DeVries <31195314+h3r0TOz3r0@users.noreply.github.com>
2019-04-08 17:40:07 -0400

    Update centos_targetenum.sh

--- centos_targetenum.sh
+++ centos_targetenum.sh
@@ -21,7 +21,7 @@ echo $ans
 echo "Target Enumeration Text File" > ${ans}_centos_attack_vector.txt
 echo " " >> ${ans}_centos_attack_vector.txt
 
-echo "-----System Information-----" >> ${ans}_centos_attack_vector.txt
+echo "--------------------System Information--------------------" >> ${ans}_centos_attack_vector.txt
 cat /etc/centos-release >> ${ans}_centos_attack_vector.txt
 echo "IP Address: " >> ${ans}_centos_attack_vector.txt
 ip addr | grep "inet " >> ${ans}_centos_attack_vector.txt
@@ -29,44 +29,31 @@ echo "Kernel Information: " >> ${ans}_centos_attack_vector.txt
 uname -a >> ${ans}_centos_attack_vector.txt
 echo " " >> ${ans}_centos_attack_vector.txt
 
-echo "-----User/Root Privs Information-----" >> ${ans}_centos_attack_vector.txt
+echo "--------------------User/Root Privs Information--------------------" >> ${ans}_centos_attack_vector.txt
 echo "Password: " >> ${ans}_centos_attack_vector.txt
 cat /etc/passwd >> ${ans}_centos_attack_vector.txt
 echo "Shadow: " >> ${ans}_centos_attack_vector.txt
 cat /etc/shadow >> ${ans}_centos_attack_vector.txt
 echo "Groups: " >> ${ans}_centos_attack_vector.txt
 cat /etc/group >> ${ans}_centos_attack_vector.txt
+echo "Logged in Users: " >> ${ans}_centos_attack_vector.txt
+who >> ${ans}_centos_attack_vector.txt
+echo " " >> ${ans}_centos_attack_vector.txt
 
+echo "--------------------Services and Ports--------------------" >> ${ans}_centos_attack_vector.txt
+netstat -pan >> ${ans}_centos_attack_vector.txt
+echo " " >> ${ans}_centos_attack_vector.txt
 
+echo "--------------------Systemctl Enabled--------------------" >> ${ans}_centos_attack_vector.txt
+systemctl list-unit-files | grep enabled >> ${ans}_centos_attack_vector.txt
+echo " " >> ${ans}_centos_attack_vector.txt
 
+echo "--------------------Firewall--------------------" >> ${ans}_centos_attack_vector.txt
+echo "IPTABLES: " >> ${ans}_centos_attack_vector.txt
+iptables -L >> ${ans}_centos_attack_vector.txt
+echo " " >> ${ans}_centos_attack_vector.txt
+echo "Firewalld: " >> ${ans}_centos_attack_vector.txt
+sudo firewall-cmd --zone=public --list-all >> ${ans}_centos_attack_vector.txt
+echo " " >> ${ans}_centos_attack_vector.txt
 
-
-
-
-
-
-echo "-------------- SUDOERS -------------" >> `echo $RESULTFILE` 2>&1
-getent group sudo | cut -d: -f4 >> `echo $RESULTFILE` 2>&1
-echo "---------- LOGGED IN USERS ---------" >> `echo $RESULTFILE` 2>&1
-who >> `echo $RESULTFILE` 2>&1
-echo "======================== NETWORKING ========================" >> `echo $RESULTFILE` 2>&1
-echo "-------------- INTERFACES --------------" >> `echo $RESULTFILE` 2>&1
-ip addr show >> `echo $RESULTFILE` 2>&1
-ip route show >> `echo $RESULTFILE` 2>&1
-echo "-------------- OPEN PORTS --------------" >> `echo $RESULTFILE` 2>&1
-netstat -pant >> `echo $RESULTFILE` 2>&1
-echo "========================= FIREWALL =========================" >> `echo $RESULTFILE` 2>&1
-iptables -L >> `echo $RESULTFILE` 2>&1
-ufw -status >> `echo $RESULTFILE` 2>&1
-echo "========================= PROCESSES ========================" >> `echo $RESULTFILE` 2>&1
-ps -ef | grep -v '\[' >> `echo $RESULTFILE` 2>&1
-echo "========================= SERVICES =========================" >> `echo $RESULTFILE` 2>&1
-systemctl list-units --type service >> `echo $RESULTFILE` 2>&1
-echo "============================ JOBS ==========================" >> `echo $RESULTFILE` 2>&1
-for user in $(cut -f1 -d: /etc/passwd); do crontab -u $user -l >> `echo $RESULTFILE` 2>&1; done
-echo "============================ LOGS ==========================" >> `echo $RESULTFILE` 2>&1
-ls -lisa /var/log >> `echo $RESULTFILE` 2>&1
-
-echo "Survey complete. file stored at $RESULTFILE"
-echo "Please move the file off by running the following command from your host machine:"
-echo "scp $logginuser@$ipaddr:$RESULTFILE ."
+echo "Target Enumeration Complete"
