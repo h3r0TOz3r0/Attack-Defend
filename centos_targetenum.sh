2c64e99843689557c32165a218e2250d30a83c61
Anna DeVries <31195314+h3r0TOz3r0@users.noreply.github.com>
2019-04-11 19:24:29 -0400

    Update centos_targetenum.sh

--- centos_targetenum.sh
+++ centos_targetenum.sh
@@ -19,10 +19,10 @@ echo -n "Which service is this box running?: "
 read ans
 echo $ans
 
-echo "Target Enumeration Text File" > ${ans}_centos_attack_vector.txt
-echo " " >>  ${ans}_centos_attack_vector.txt
+echo "Target Enumeration Text File" >&2 ${ans}_centos_attack_vector.txt
+echo " " >>&2  ${ans}_centos_attack_vector.txt
 
-echo "--------------------System Information--------------------" >> ${ans}_centos_attack_vector.txt
+echo "--------------------System Information--------------------" >>&2 ${ans}_centos_attack_vector.txt
 cat /etc/centos-release >> ${ans}_centos_attack_vector.txt
 echo "IP Address: " >> ${ans}_centos_attack_vector.txt
 ip addr | grep "inet " >> ${ans}_centos_attack_vector.txt
@@ -30,11 +30,11 @@ echo "Kernel Information: " >> ${ans}_centos_attack_vector.txt
 uname -a >> ${ans}_centos_attack_vector.txt
 echo " " >> ${ans}_centos_attack_vector.txt
 
-echo "--------------------Services and Ports--------------------" >> ${ans}_centos_attack_vector.txt
+echo "--------------------Services and Ports--------------------" >>&2 ${ans}_centos_attack_vector.txt
 netstat -pan >> ${ans}_centos_attack_vector.txt
 echo " " >> ${ans}_centos_attack_vector.txt
 
-echo "--------------------User/Root Privs Information--------------------" >> ${ans}_centos_attack_vector.txt
+echo "--------------------User/Root Privs Information--------------------" >>&2 ${ans}_centos_attack_vector.txt
 echo "Password: " >> ${ans}_centos_attack_vector.txt
 cat /etc/passwd >> ${ans}_centos_attack_vector.txt
 echo "Shadow: " >> ${ans}_centos_attack_vector.txt
@@ -45,7 +45,7 @@ echo "Logged in Users: " >> ${ans}_centos_attack_vector.txt
 who >> ${ans}_centos_attack_vector.txt
 echo " " >> ${ans}_centos_attack_vector.txt
 
-echo "--------------------Firewall--------------------" >> ${ans}_centos_attack_vector.txt
+echo "--------------------Firewall--------------------" >>&2 ${ans}_centos_attack_vector.txt
 echo "IPTABLES: " >> ${ans}_centos_attack_vector.txt
 iptables -L >> ${ans}_centos_attack_vector.txt
 echo " " >> ${ans}_centos_attack_vector.txt
@@ -53,7 +53,7 @@ echo "Firewalld: " >> ${ans}_centos_attack_vector.txt
 sudo firewall-cmd --zone=public --list-all >> ${ans}_centos_attack_vector.txt
 echo " " >> ${ans}_centos_attack_vector.txt
 
-echo "--------------------Logs--------------------" >> ${ans}_centos_attack_vector.txt
+echo "--------------------Logs--------------------" >>&2 ${ans}_centos_attack_vector.txt
 journalctl -xe >> ${ans}_centos_attack_vector.txt
 echo " " >> ${ans}_centos_attack_vector.txt
 
